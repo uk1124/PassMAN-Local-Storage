bd6a26b4c2440705945cabe6358896fde9e890fd
unknown <utkarshshukla1124@gmail.com>
2024-04-24 20:13:27 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-slate-800 text-white flex flex-col justify-center items-center  w-full">
+      <div className="logo font-bold text-white text-2xl">
+        <span className="text-violet-500"> &lt;</span>
+        <span>Pass</span>
+        <span className="text-violet-500">MAN/&gt;</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-slate-800 text-white ">
+      <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
+        <div className="logo font-bold text-white text-2xl">
+          <span className="text-violet-500"> &lt;</span>
+          <span>Pass</span>
+          <span className="text-violet-500">MAN/&gt;</span>
+        </div>
+        {/* <ul>
+                    <li className='flex gap-4 '>
+                        <a className='hover:font-bold' href='/'>Home</a>
+                        <a className='hover:font-bold' href='#'>About</a>
+                        <a className='hover:font-bold' href='#'>Contact</a>
+                    </li>
+                </ul> */}
+        <button
+          className="text-white bg-violet-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1"
+          onClick={() =>
+            window.open("https://github.com/uk1124/PassMAN", "_blank")
+          }
+        >
+          <img
+            className="invert  w-10 p-1"
+            src="/icons/github.svg"
+            alt="github logo"
+          />
+          <span className="font-bold px-2">GitHub</span>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
